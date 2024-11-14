69e5a0465387b665a7a4fa70062b2ca6e18a7c33
Java2303 <java230305@gmail.com>
2024-11-14 12:17:09 -0400

    agregar 3 paginas

---
+++ git1/Pages/Contactos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace git1.Pages
+{
+    public class ContactosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ git1/Pages/Servicios.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace git1.Pages
+{
+    public class ServiciosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
